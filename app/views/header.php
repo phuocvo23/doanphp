290071de54898ba8c455e24bf1d3f5b828750f1d
phuocvo23 <phuocvo23@LAPTOP-T3D2AE0I>
2024-03-25 15:45:57 +0700

    ssss

---
+++ app/views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <div class="header">
+            <!-- sdaasda -->
+            <a href="#"> 
+                <img src="" alt="">
+            </a>
+            
+            <a href="#">
+                HOME
+            </a>
+
+            <a href="#">
+                ABOUT
+            </a>
+
+            <a href="">
+
+            </a>
+        </div>        
+    </body>
+</html>
