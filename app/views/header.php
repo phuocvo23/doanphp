04c33746627362728c7085e2d6390599e9e06e93
7binh <87206514+7binh@users.noreply.github.com>
2024-03-25 16:27:39 +0700

    đã xong header

--- app/views/header.php
+++ app/views/header.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <body>
-        <div class="header">
-            <!-- sdaasda -->
-            <a href="#"> 
-                <img src="" alt="">
-            </a>
-            
-            <a href="#">
-                HOME
-            </a>
-
-            <a href="#">
-                ABOUT
-            </a>
-
-            <a href="">
-
-            </a>
-        </div>        
-    </body>
-</html>
+<header class="header">
+    <div class="container">
+        <div class="header-container">
+            <div class="header-logo">
+                <img src="./public/image/apple_logo.svg" alt="" class="header-logo-image">
+            </div>
+            <div class="header-nav">
+                <a href="#" class="header-nav-item">Product</a>
+                <a href="#" class="header-nav-item">Contact</a>
+                <a href="#" class="header-nav-item">Support</a>
+                <a href="#" class="header-nav-item">About</a>
+            </div>
+            <div class="header-auth">
+                <a href="#" class="header-auth-login btn btn--primary">Login</a>
+                <a href="#" class="header-auth-register btn btn--secondary">Sign up</a>
+            </div>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,24 +1,24 @@
+<?php require_once "app/views/header.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/css/view.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>Document</title>
-    
 </head>
+
 <body>
-    <header class="header">
-        <div class="container">
-            <a href="">
-                <img src="" alt="">
-            </a>
-        </div>
-    </header>
-
-    <section></section>
-
-    
+    <?php require_once "app/views/header.php"; ?>
+    <?php
+    // sass ./public/css/view.scss ./public/css/view.css --watch
+    require_once "app/views/footer.php";
+    ?>
 </body>
+
 </html>
